826f314c0098dcaf99b40af08f721d46e442ea87
Gabriel Ryan <ryangabriel.gr77@gmail.com>
2026-03-31 23:20:16 -0300

    Organizando estrutura final com backend e frontend

--- back-end/meu-projeto-springboot-1/src/main/java/com/app/model/PerfilUsuario.java
+++
@@ -1,7 +0,0 @@
-package com.app.model;
-
-public enum PerfilUsuario {
-		CAIXA,
-		GERENTE;
-
-}
--- back-end/meu-projeto-springboot-1/src/main/java/com/app/repository/VendaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.model.Venda;
-
-public interface VendaRepository extends JpaRepository<Venda, Long> {
-
-}
